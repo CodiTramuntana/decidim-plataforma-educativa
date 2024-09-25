b5799c563347ad40c2ce203f876af70991612361
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2024-09-25 12:14:58 +0200

    Add pending crons

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,18 +2,26 @@
 
 env :PATH, ENV.fetch("PATH", nil)
 
+every 1.day, at: "3:00 am" do
+  rake "decidim_meetings:clean_registration_forms"
+end
+
+every :monday, at: "3:30 am" do
+  rake "tmp:clear"
+end
+
+every 1.day, at: "4:00 am" do
+  rake "decidim:reminders:all"
+end
+
+every :sunday, at: "4:30 am" do
+  rake "decidim:delete_data_portability_files"
+end
+
 every 1.day, at: "5:00 am" do
   rake "decidim:metrics:all"
 end
 
 every 1.day, at: "6:00 am" do
   rake "decidim:open_data:export"
-end
-
-every :sunday, at: "4:00 am" do
-  rake "decidim:delete_data_portability_files"
-end
-
-every :monday, at: "3:00 am" do
-  rake "tmp:clear"
 end
